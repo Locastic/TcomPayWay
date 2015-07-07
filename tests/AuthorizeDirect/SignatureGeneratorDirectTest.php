b38aec004ae13234c157ee49d3412dada937d954
Antonio Peric <antonio@locastic.com>
2015-07-07 10:51:52 +0200

    FIxed path for tests

--- tests/AuthorizeDirect/SignatureGeneratorDirectTest.php
+++ tests/AuthorizeDirect/SignatureGeneratorDirectTest.php
@@ -11,30 +11,58 @@ class SignatureGeneratorDirectTest extends PHPUnit_Framework_TestCase
     public function testGetSignature()
     {
         $model = new Payment(
-            123,
+            12345,
             'secretkey',
-            'narudžba456',
-            789,
+            'order-123',
+            102400,
             0,
-            'http://www.mojducan.com/success/narudžba456',
-            'http://www.mojducan.com/failure/narudžba456',
+            'http://www.mojducan.com/success/order-123',
+            'http://www.mojducan.com/failure/order-123',
             '111111111111111',
             '1812',
             '123',
-            'Test',
-            'Testic',
-            'Testna bb',
-            'Testograd',
-            12345,
-            'Testozemska',
-            'email@email.com'
+            'John',
+            'Smith',
+            'Street 49',
+            'Locastic City',
+            '1950',
+            'LocasticLand',
+            'email@example.com'
         );
 
-        $model->setPgwPhoneNumber('012345678');
+        $this->assertEquals(
+            'f0a8e275979fe2da37c80d6dd668f47aa0510539134dd9a68eb8e75ffa841c0523489f8ddffd25baa9b6105c95aad098aab08e84cbfc4fca47fafab210bffd54',
+            SignatureGenerator::getSignature('secretkey', $model)
+        );
+
+        $model = new Payment(
+            123456789,
+            'new-secret-key',
+            'order-no-135',
+            195000,
+            1,
+            'http://www.mojducan.com/success/order-no-135',
+            'http://www.mojducan.com/failure/order-no-135',
+            '111111111111111',
+            '1812',
+            '123',
+            'John',
+            'Smith',
+            'Street 49',
+            'Locastic City',
+            '1911',
+            'LocasticLand',
+            'email@example.com'
+        );
+
+        $model->setPgwReturnMethod('get');
+        $model->setPgwInstallments('12');
+        $model->setPgwPhoneNumber('098123456789');
+        $model->setPgwMerchantData('item1|item2|item3');
 
         $this->assertEquals(
-            '67837faa88d323ab6cd61502bd930adc524a5da8e3849cde2b5bc18561306f50d4cb862e4a26ea1aa019d60ceced2da503d2c5fcd7391ea20d4a16158cde51d4',
-            SignatureGenerator::getSignature('secretkey', $model)
+            '7ab8d368e0b1143f05f56d0f5f65d3dbbd101f9cd824587934225d141fa7806746afd5258679665d1fd876ff33f1c0273a7c81fab9340b4535cdc9dc7ca713a6',
+            SignatureGenerator::getSignature('new-secret-key', $model)
         );
     }
 }
--- tests/AuthorizeForm/SignatureGeneratorFormTest.php
+++ tests/AuthorizeForm/SignatureGeneratorFormTest.php
@@ -21,8 +21,32 @@ class SignatureGeneratorFormTest extends PHPUnit_Framework_TestCase
         );
 
         $this->assertEquals(
-            '8295bfece351e248e73870ad10ffb9dc63abd807582e5fdd4348d12284f6b8cc13e93eaa502034c1cb4114ddc84f19868d4ebfff55682e0c521a96a5022974cb',
+            '178b4e0f865f0576857b54cbdcb1b88871792ba976e1ca707f2eb10f4e7cef00e9e6526a2f72bb2584f66c73c6805f062ce8da56d6a3cfe9edfa32ec75bf615a',
             SignatureGenerator::getSignature('secretkey', $model)
+        );
+
+        $model = new Payment(
+            123,
+            'new-secret-key',
+            'narudžba456',
+            789,
+            1,
+            'http://www.mojducan.com/success/narudžba456',
+            'http://www.mojducan.com/failure/narudžba456'
+        );
+
+        $model->setPgwFirstName('John');
+        $model->setPgwLastName('Smith');
+        $model->setPgwStreet('Some Street 49');
+        $model->setPgwCity('Split');
+        $model->setPgwEmail('email@example.com');
+        $model->setPgwDisableInstallments(1);
+        $model->setPgwAuthorizationType(1);
+        $model->setPgwAuthorizationToken('SomeTOken123');
+
+        $this->assertEquals(
+            'eaa9e7587db7751fc6b7cd97f9d5ea735dece3c115fc344ebf85d8d65c462764ec1cf220c55e8b69313f787eb8f2a13e522558b747e3842617eb70ca2dd128cc',
+            SignatureGenerator::getSignature('new-secret-key', $model)
         );
     }
 }
